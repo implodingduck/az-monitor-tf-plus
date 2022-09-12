a21b91b1b6cb51d7c6b6dc73fdc3347d4576b256
implodingduck <zsek320@gmail.com>
2022-09-12 11:51:12 -0500

    adding fail path

--- func/HttpTrigger/index.js
+++ func/HttpTrigger/index.js
@@ -5,7 +5,9 @@ module.exports = async function (context, req) {
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    if ( name && name.toLowerCase().indexOf('fail') > 0){
+        throw new Error('This is an error because the name contains fail...')
+    }
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
