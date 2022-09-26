77b703b75167886091524fdcb6b1858ee56538a3
implodingduck <zsek320@gmail.com>
2022-09-26 09:48:30 -0500

    adding grafana dashboard

--- terraform/main.tf
+++ terraform/main.tf
@@ -1126,3 +1126,18 @@ resource "azurerm_portal_dashboard" "board" {
 }
 DASH
 }
+
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                              = "${local.func_name}-grafana"
+  resource_group_name               = azurerm_resource_group.rg.name
+  location                          = azurerm_resource_group.rg.location
+  api_key_enabled                   = true
+  deterministic_outbound_ip_enabled = true
+  public_network_access_enabled     = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.tags
+}
