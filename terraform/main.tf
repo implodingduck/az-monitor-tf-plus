9af873868ffaaeabe9d6ea0357ce0c70d6d43d87
implodingduck <zsek320@gmail.com>
2022-11-01 16:30:55 -0500

    removing extra space

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,7 +117,7 @@ resource "azurerm_linux_function_app" "func" {
 
 resource "azurerm_role_assignment" "func_mmp" {
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  role_definition_name = " Monitoring Metrics Publisher"
+  role_definition_name = "Monitoring Metrics Publisher"
   principal_id         = azurerm_linux_function_app.func.identity.0.principal_id
 }
 
