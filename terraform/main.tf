995afcca0f760ffc2c9ec9c9138b09438451f4fb
implodingduck <zsek320@gmail.com>
2022-11-01 14:59:33 -0500

    fixing scope

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,7 +113,7 @@ resource "azurerm_linux_function_app" "func" {
 
 
 resource "azurerm_role_assignment" "func_mmp" {
-  scope                = "/providers/Microsoft.Subscription/${data.azurerm_client_config.current.subscription_id}"
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   role_definition_name = " Monitoring Metrics Publisher"
   principal_id         = azurerm_linux_function_app.func.identity.0.principal_id
 }
