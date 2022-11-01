eac626570484fd5877afe18adb852f6261fd8a97
implodingduck <zsek320@gmail.com>
2022-11-01 14:01:47 -0500

    adding context log of body

--- func/HttpTrigger/index.js
+++ func/HttpTrigger/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+    context.log(req.body);
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
@@ -9,6 +9,7 @@ module.exports = async function (context, req) {
         context.log('name contains fail...')
         throw new Error('This is an error because the name contains fail...')
     }
+    
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
