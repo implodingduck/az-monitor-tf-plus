d512a320ef8ae2014f4e301d4ac6575aa453917d
implodingduck <zsek320@gmail.com>
2022-11-01 20:48:43 -0500

    adjusting due to response object

--- func/HttpTrigger/index.js
+++ func/HttpTrigger/index.js
@@ -23,14 +23,10 @@ module.exports = async function (context, req) {
             }
         ]
         const result = await client.upload(ruleId, streamName, logs);
-        if (result.uploadStatus !== "Success") {
-            context.log("Some logs have failed to complete ingestion. Upload status=", result.uploadStatus);
+        if (result.status !== "Success") {
+            context.log("Some logs have failed to complete ingestion. Upload status=", result.status);
             context.log(result)
-            context.log(JSON.stringify(result))
-            for (const errors of result.errors) {
-                context.log(`Error - ${JSON.stringify(errors.responseError)}`);
-                context.log(`Log - ${JSON.stringify(errors.failedLogs)}`);
-            }
+            
         }
 
     }else {
